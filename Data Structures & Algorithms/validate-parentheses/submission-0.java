4c0903eda92c337a8d0e9c46ac5a97474f6926ae
bb-code1 <barkatbashir10@gmail.com>
2026-06-29 10:27:04 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    private boolean bruteForceSolution(String s){
+        
+    while(s.contains("()") || s.contains("{}") || s.contains("[]") ){
+            s= s.replace("()","");
+           s=  s.replace("{}","");
+           s=  s.replace("[]","");
+        }
+        return s.isEmpty();
+    }
+    public boolean isValid(String s) {
+        return bruteForceSolution(s);
+        
+    }
+}
